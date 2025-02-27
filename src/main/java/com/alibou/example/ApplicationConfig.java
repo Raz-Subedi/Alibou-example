fd719963fb65ab089eeb3b0e7e8b0e5ddb296a09
Raz Subedi <subedirazkumar@gmail.com>
2025-02-27 22:49:56 +0545

    Profile

--- src/main/java/com/alibou/example/ApplicationConfig.java
+++ src/main/java/com/alibou/example/ApplicationConfig.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("dev")
 public class ApplicationConfig {
 
     @Bean
